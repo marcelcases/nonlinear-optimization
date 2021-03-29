6b8b8c016588a06a770db9a09c27dcf102f4fb1d
Marcel Cases i Freixenet <m@rcel.xyz>
2021-03-29 21:40:04 +0200

    constraint format normalised
    Signed-off-by: Marcel Cases i Freixenet <m@rcel.xyz>

--- ex4.py
+++ ex4.py
@@ -13,18 +13,19 @@ print('\nSOLVING USING CVXPY\n')
 # Create two scalar optimization variables.
 x = cp.Variable(1, name='x')
 
-# Constraints
-f1 = (x - 2)*(x - 4)
-constraints = [f1<=0.]
-
 # Form objective.
 f0 = x**2 + 1
 obj = cp.Minimize(f0)
+
+# Constraints
+# f1 = (x - 2)*(x - 4)
+f1 = x**2 - 6*x + 8
+constraints = [f1<=0.]
 
 # Form and solve problem.
 prob = cp.Problem(obj, constraints)
 print("solve", prob.solve())  # Returns the optimal value.
 print("status:", prob.status)
 print("optimal value p* = ", prob.value)
-print("optimal var: x1 = ", x.value)
-print("optimal dual variables lambda1 = ", constraints[0].dual_value)
+print("optimal var: x = ", x.value)
+print("optimal dual variables lambda = ", constraints[0].dual_value)
